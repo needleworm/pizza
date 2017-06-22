2891efa5c309f4bc094cf3cc4d73e4159915c909
bhban <needleworm@kaist.ac.kr>
2017-06-22 22:03:00 +0900

    why?

--- main.py
+++ main.py
@@ -62,21 +62,6 @@ dropout_rate = 0.5
 lr_decay = 1/1.15
 
 
-class LargeConfig(object):
-  """Large config."""
-  init_scale = 0.04
-  learning_rate = 1.0
-  max_grad_norm = 10
-  num_layers = 2
-  num_steps = 35
-  hidden_size = 1500
-  max_epoch = 14
-  max_max_epoch = 55
-  keep_prob = 0.35
-  lr_decay = 1 / 1.15
-  batch_size = 20
-  vocab_size = 10000
-
 
 def run_epoch(session, model, eval_op=None, verbose=False):
     start_time = time.time()
