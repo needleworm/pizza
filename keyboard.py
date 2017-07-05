056f13f78ea5946449b704fa3205a992dc7fd093
madong <edgus123@kaist.ac.kr>
2017-07-05 13:27:26 +0900

    keyboard module beta

--- keyboard.py
+++ keyboard.py
@@ -1,9 +1,9 @@
 import pygame
 import numpy as np
 
-class Keyboard:
+class Keyboard():
 
-    def __init__(self,size=128):
+    def __init__(self,size = 128, length = 300):
         """
             size = midi tensor row size (default = 128)
         """
@@ -14,62 +14,93 @@ class Keyboard:
                     (53, 64), (116,65), (121,66), (55, 67), (117, 68), (56, 69), (105, 70),
                     (57,71), (111, 72), (112, 73)])
         self.motive = np.zeros((size,300),dtype=bool)
+        self.line = np.zeros((size,1), dtype = bool)
+        self.run = True
+        self.size = size
+        self.length = length
+        self.sequence = 0
+        pygame.init()
+        self.clock = pygame.time.Clock()
+     
+    def _wait(self, time):
 
-    def start():
+        return pygame.time.wait(time) 
 
-        print("start")
+    def _get(self):
 
-    def merge():
-
-        print("merge")
-
-    def process():
-
-        print("process")
-
-
-
- 
-pygame.init()
- 
-
- 
-clock = pygame.time.Clock()
-run = True
+        self.line = np.zeros((self.size, 1), dtype = bool)
 
 
-ascii2midi = dict([(113, 58), (50, 59), (119, 60), (101, 61), (52, 62), (114, 63), 
-                    (53, 64), (116,65), (121,66), (55, 67), (117, 68), (56, 69), (105, 70),
-                    (57,71), (111, 72), (112, 73)])
-
-motive = np.zeros((128,300),dtype=bool)
-sequence = 0
-while run:
- 
-    count = 0
-    #pygame.event.get() : 키를 눌렀을때 이벤트
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE: #esc 누르면 종
-                run = False
- 
- 
-    #pygame.key.get_pressed() - 전체 키배열중 현재 눌려져있는 키를 bool형식의 튜플로 반환
-    keys = pygame.key.get_pressed()
-    #print(len(keys));
-    for keyboard_index in range(len(keys)):
-        if keys[keyboard_index] == 1:
-            count = count + 1
-            if keyboard_index in ascii2midi:
-                motive[ascii2midi[keyboard_index],sequence] = True
-
-    print(motive[:,sequence]) 
+        keys = pygame.key.get_pressed()
+        for keyboard_index in range(len(keys)):
+            if keys[keyboard_index] == 1:
+                if keyboard_index in self.ascii2midi:
+                    self.line[self.ascii2midi[keyboard_index],0] = True
+       
+        print(self.line.T)
+        return self.line.T
     
-    if sequence == 299:
-        run = False
-    sequence = sequence + 1
-    pygame.time.wait(60)
-    #print(pygame.time.get_ticks())
-    #clock.tick(16)
- 
-pygame.quit()
+    def _get_motive(self):
+
+        self.run = True
+        self.sequence = 0
+        self.motive = np.zeros((self.size, self.length), dtype = bool)
+
+        while self.run:
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE: #esc 누르면 종
+                        self.run = False
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.run = False
+            
+
+
+            if self.sequence == self.length:
+                self.run = False
+                break
+
+            self.motive[:,self.sequence] = self._get()
+            self.sequence = self.sequence + 1
+            self._wait(60)
+
+        return self.motive
+
+    def start(self):
+
+
+        """
+           key board input start
+
+           press ESC for quit
+            
+
+        """
+
+        while self.run:
+
+            self.tensor = np.zeros((self.size,1),dtype=bool)
+
+            for event in pygame.event.get():
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_ESCAPE: #esc 누르면 종
+                        self.run = False
+                
+                if event.type == pygame.KEYDOWN:
+                    if event.key == pygame.K_LEFT:
+                        self.run = False
+            
+
+            keys = pygame.key.get_pressed()
+            for keyboard_index in range(len(keys)):
+                if keys[keyboard_index] == 1:
+                    if keyboard_index in self.ascii2midi:
+                        self.tensor[self.ascii2midi[keyboard_index],0] = True
+                        self.motive[self.ascii2midi[keyboard_index],self.sequence] = True
+            
+            pygame.time.wait(60)
+
+        pygame.quit()
