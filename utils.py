7b7671688c678b0a4813faffee2557113c6e6c3a
nobleminsu <nobleminsu@gmail.com>
2017-07-13 12:55:48 +0900

    recursive validation

--- utils.py
+++ utils.py
@@ -5,9 +5,7 @@
     Latest Modification : 7/3, 2017
 """
 
-import tensorflow as tf
 import numpy as np
-from PIL import Image
 
 import tensor2midi
 
@@ -17,37 +15,54 @@ def run_epoch(dataset, batch_size, model, session, dropout_rate, began_loss=True
     feed_dict = {model.input_music_seg: hidden_state,
                  model.ground_truth_seg: ground_truth,
                  model.keep_probability: dropout_rate}
-    
+
     if began_loss:
         train_op, train_op_g = session.run([model.train_op, model.train_op_g], feed_dict=feed_dict)
     else:
         train_op = session.run(model.train_op, feed_dict=feed_dict)
-    
+
     return feed_dict
-    
-    
-def validation(dataset, batch_size, model, hidden_state_size, predict_size, session, logs_dir, idx, tick_interval, began_loss=True):
+
+
+def validation(dataset, batch_size, model, hidden_state_size, predict_size, session, logs_dir, idx, tick_interval,
+               began_loss=True):
     hidden_state, ground_truth = dataset.next_batch()
     feed_dict = {model.input_music_seg: hidden_state,
-                  model.ground_truth_seg: ground_truth,
-                  model.keep_probability: 1.0}
-    
+                 model.ground_truth_seg: ground_truth,
+                 model.keep_probability: 1.0}
+
     if began_loss:
         loss_d, loss_g, predict = session.run([model.loss, model.loss_g, model.predict], feed_dict=feed_dict)
     else:
-        loss_d, predict = session.run([model.loss, model.predict], feed_dict=feed_dict) 
+        loss_d, predict = session.run([model.loss, model.predict], feed_dict=feed_dict)
 
-    save_music(hidden_state, predict, logs_dir+"/out_midi/", "VALIDATION_MUSICS_" + str(idx).zfill(5), batch_size, tick_interval)
+    save_music(hidden_state, predict, logs_dir + "/out_midi/", "VALIDATION_MUSICS_" + str(idx).zfill(5), batch_size,
+               tick_interval)
 
-    
     if began_loss:
         return loss_d, loss_g, predict
     else:
         return loss_d, predict
-        
-        
+
+
+def recursive_validation(line, batch_size, model, hidden_state_size, predict_size, session, logs_dir, idx,
+                         tick_interval):
+    hidden_state = line[-hidden_state_size:]
+    feed_dict = {model.input_music_seg: hidden_state, model.keep_probability: 1.0}
+
+    predict = session.run([model.loss, model.predict], feed_dict=feed_dict)
+
+    save_music(hidden_state, predict, logs_dir + "/out_midi/", "REC_VALIDATION_MUSICS_" + str(idx).zfill(5), batch_size,
+               tick_interval)
+
+    line.extend(predict)
+
+    return line
+
+
 def save_music(hidden_state, predict, path, name, batch_size, tick_interval):
     merged = np.concatenate((hidden_state, predict), axis=2)
+
     print(merged[0].shape)
 
     for i in range(batch_size):
