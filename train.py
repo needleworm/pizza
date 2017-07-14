48733d745f99c525c56bd78091d7c18296ca00dd
nobleminsu <nobleminsu@gmail.com>
2017-07-14 22:02:47 +0900

    is_batch_zero_pad를 input으로 받아서 zero padding을 처리하도록 수정

--- train.py
+++ train.py
@@ -5,17 +5,16 @@
     Latest Modification : 7/3, 2017
 """
 
-import tensorflow as tf
-import midi2tensor as mt
-import numpy as np
 import os
 import sys
+
+import tensorflow as tf
+
+import midi2tensor as mt
 import model as G
-import time
 import utils
 
 __author__ = "BHBAN"
-
 
 FLAGS = tf.flags.FLAGS
 
@@ -26,6 +25,7 @@ tf.flags.DEFINE_string('device_test', '/gpu:0', "device : /cpu:0 /gpu:0 /gpu:1 [
 tf.flags.DEFINE_bool('debug', "False", "debug mode : True/ False [default : True]")
 tf.flags.DEFINE_bool('reset', "True", "reset : True/False")
 tf.flags.DEFINE_bool('use_began_loss', "True", "began loss? : True/False")
+tf.flags.DEFINE_bool("is_batch_zero_pad", "True", "batch is zero pad? : True/False")
 tf.flags.DEFINE_integer('hidden_state_size', "300", "window size. [default : 100]")
 tf.flags.DEFINE_integer('predict_size', "300", "window size. [default : 10]")
 tf.flags.DEFINE_integer("tr_batch_size", "512", "batch size for training. [default : 100]")
@@ -61,9 +61,7 @@ dropout_rate = 0.5
 tick_interval = 0.03
 
 
-
 def GAN():
-
     #                               Graph Part                                 #
     print("Graph initialization...")
     with tf.device(FLAGS.device_train):
@@ -88,12 +86,12 @@ def GAN():
     with tf.device(FLAGS.device_test):
         with tf.variable_scope("model", reuse=True):
             m_test = G.GAN(batch_size=FLAGS.test_batch_size,
-                            is_training=False,
-                            num_keys=FLAGS.num_keys,
-                            input_length=FLAGS.hidden_state_size,
-                            output_length=FLAGS.predict_size,
-                            learning_rate=learning_rate,
-                            use_began_loss=FLAGS.use_began_loss)
+                           is_training=False,
+                           num_keys=FLAGS.num_keys,
+                           input_length=FLAGS.hidden_state_size,
+                           output_length=FLAGS.predict_size,
+                           learning_rate=learning_rate,
+                           use_began_loss=FLAGS.use_began_loss)
     print("Done")
 
     #                               Summary Part                               #
@@ -114,8 +112,22 @@ def GAN():
 
     #                               Session Part                               #
     print("Setting up Data Reader...")
-    validation_dataset_reader = mt.Dataset(test_dir, FLAGS.val_batch_size, FLAGS.hidden_state_size, FLAGS.predict_size, FLAGS.num_keys, tick_interval, step=FLAGS.slice_step)
-    test_dataset_reader = mt.Dataset(test_dir, FLAGS.test_batch_size, FLAGS.hidden_state_size, FLAGS.predict_size, FLAGS.num_keys, tick_interval, step=FLAGS.slice_step)
+    validation_dataset_reader = mt.Dataset(directory=test_dir,
+                                           batch_size=FLAGS.val_batch_size,
+                                           is_batch_zero_pad=FLAGS.is_batch_zero_pad,
+                                           hidden_state_size=FLAGS.hidden_state_size,
+                                           predict_size=FLAGS.predict_size,
+                                           num_keys=FLAGS.num_keys,
+                                           tick_interval=tick_interval,
+                                           step=FLAGS.slice_step)
+    test_dataset_reader = mt.Dataset(directory=test_dir,
+                                     batch_size=FLAGS.test_batch_size,
+                                     is_batch_zero_pad=FLAGS.is_batch_zero_pad,
+                                     hidden_state_size=FLAGS.hidden_state_size,
+                                     predict_size=FLAGS.predict_size,
+                                     num_keys=FLAGS.num_keys,
+                                     tick_interval=tick_interval,
+                                     step=FLAGS.slice_step)
     print("done")
 
     sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
@@ -129,25 +141,33 @@ def GAN():
         sess.run(tf.global_variables_initializer())  # if the checkpoint doesn't exist, do initialization
 
     if FLAGS.mode == "train":
-        train_dataset_reader = mt.Dataset(train_dir, FLAGS.tr_batch_size, FLAGS.hidden_state_size, FLAGS.predict_size, FLAGS.num_keys, tick_interval, step=FLAGS.slice_step)
+        train_dataset_reader = mt.Dataset(directory=train_dir,
+                                          batch_size=FLAGS.tr_batch_size,
+                                          is_batch_zero_pad=FLAGS.is_batch_zero_pad,
+                                          hidden_state_size=FLAGS.hidden_state_size,
+                                          predict_size=FLAGS.predict_size,
+                                          num_keys=FLAGS.num_keys,
+                                          tick_interval=tick_interval,
+                                          step=FLAGS.slice_step)
         for itr in range(MAX_MAX_EPOCH):
-            feed_dict = utils.run_epoch(train_dataset_reader, FLAGS.tr_batch_size, m_train, sess, dropout_rate, began_loss=FLAGS.use_began_loss)
+            feed_dict = utils.run_epoch(train_dataset_reader, FLAGS.tr_batch_size, m_train, sess, dropout_rate,
+                                        began_loss=FLAGS.use_began_loss)
 
             if itr % 100 == 0:
                 if FLAGS.use_began_loss:
-                    train_loss_d, train_loss_g, train_pred = sess.run([m_train.loss, m_train.loss_g, m_train.predict], feed_dict = feed_dict)
+                    train_loss_d, train_loss_g, train_pred = sess.run([m_train.loss, m_train.loss_g, m_train.predict],
+                                                                      feed_dict=feed_dict)
                     train_summary_str_d, train_summary_str_g = sess.run([loss_summary_op_d, loss_summary_op_g],
-                                                                    feed_dict={g_loss_ph: train_loss_g,
-                                                                               d_loss_ph: train_loss_d})
-                    train_summary_writer.add_summary(train_summary_str_g , itr)
-                    print("Step : %d  TRAINING LOSS *****************" %(itr))
-                    print("Dicriminator_loss: %g\nGenerator_loss: %g" %(train_loss_d, train_loss_g))
+                                                                        feed_dict={g_loss_ph: train_loss_g,
+                                                                                   d_loss_ph: train_loss_d})
+                    train_summary_writer.add_summary(train_summary_str_g, itr)
+                    print("Step : %d  TRAINING LOSS *****************" % (itr))
+                    print("Dicriminator_loss: %g\nGenerator_loss: %g" % (train_loss_d, train_loss_g))
                 else:
                     train_loss, train_pred = sess.run([m_train.loss, m_train.predict], feed_dict=feed_dict)
-                    train_summary_str_d = sess.run(loss_summary_op_d, feed_dict={d_loss_ph:train_loss})
+                    train_summary_str_d = sess.run(loss_summary_op_d, feed_dict={d_loss_ph: train_loss})
                     train_summary_writer.add_summary(train_summary_str_d, itr)
-                    print("Step : %d  TRAINING LOSS %g" %(itr, train_loss))
-                    
+                    print("Step : %d  TRAINING LOSS %g" % (itr, train_loss))
 
             if itr % 1000 == 0:
                 if FLAGS.use_began_loss:
@@ -155,44 +175,44 @@ def GAN():
                                                                               FLAGS.val_batch_size, m_valid,
                                                                               FLAGS.hidden_state_size,
                                                                               FLAGS.predict_size, sess,
-                                                                              logs_dir, itr, tick_interval, FLAGS.use_began_loss)
+                                                                              logs_dir, itr, tick_interval,
+                                                                              FLAGS.use_began_loss)
                     valid_summary_str_d, valid_summary_str_g = sess.run([loss_summary_op_d, loss_summary_op_g],
-                                                                         feed_dict={g_loss_ph: valid_loss_g,
-                                                                                    d_loss_ph: valid_loss_d})
+                                                                        feed_dict={g_loss_ph: valid_loss_g,
+                                                                                   d_loss_ph: valid_loss_d})
                     valid_summary_writer.add_summary(valid_summary_str_d, itr)
-                    print("Step : %d  VALIDATION LOSS ***************" %(itr))
-                    print("Dicriminator_loss: %g\nGenerator_loss: %g" %(valid_loss_d, valid_loss_g))
+                    print("Step : %d  VALIDATION LOSS ***************" % (itr))
+                    print("Dicriminator_loss: %g\nGenerator_loss: %g" % (valid_loss_d, valid_loss_g))
                 else:
                     valid_loss, valid_pred = utils.validation(validation_dataset_reader,
                                                               FLAGS.val_batch_size, m_valid,
                                                               FLAGS.hidden_state_size,
                                                               FLAGS.predict_size, sess,
                                                               logs_dir, itr, tick_interval, FLAGS.use_began_loss)
-                    valid_summary_str_d = sess.run(loss_summary_op_d, feed_dict={d_loss_ph:train_loss})
+                    valid_summary_str_d = sess.run(loss_summary_op_d, feed_dict={d_loss_ph: train_loss})
                     valid_summary_writer.add_summary(valid_summary_str_d, itr)
-                    print("Step : %d  VALIDATION LOSS %g" %(itr, valid_loss))
+                    print("Step : %d  VALIDATION LOSS %g" % (itr, valid_loss))
 
             if itr % 1000 == 0:
-                utils.test_model(test_dataset_reader, 
-                         FLAGS.test_batch_size, m_test, 
-                         FLAGS.predict_size,
-                         sess, 
-                         logs_dir, 
-                         itr,
-                         tick_interval, 10)
+                utils.test_model(test_dataset_reader,
+                                 FLAGS.test_batch_size, m_test,
+                                 FLAGS.predict_size,
+                                 sess,
+                                 logs_dir,
+                                 itr,
+                                 tick_interval, 10)
 
             if itr % 1000 == 0:
                 saver.save(sess, logs_dir + "/model.ckpt", itr)
 
     if FLAGS.mode == "test":
-        utils.test_model(test_dataset_reader, 
-                         FLAGS.test_batch_size, m_test, 
+        utils.test_model(test_dataset_reader,
+                         FLAGS.test_batch_size, m_test,
                          FLAGS.predict_size,
-                         sess, 
-                         logs_dir, 
+                         sess,
+                         logs_dir,
                          9999,
                          tick_interval, 10)
-                
 
 
 def VAE():
@@ -217,11 +237,11 @@ def VAE():
 
         with tf.variable_scope("model", reuse=True):
             m_test = G.VAE(batch_size=FLAGS.test_batch_size,
-                            is_training=False,
-                            num_keys=FLAGS.num_keys,
-                            input_length=FLAGS.hidden_state_size,
-                            output_length=FLAGS.predict_size,
-                            learning_rate=learning_rate)
+                           is_training=False,
+                           num_keys=FLAGS.num_keys,
+                           input_length=FLAGS.hidden_state_size,
+                           output_length=FLAGS.predict_size,
+                           learning_rate=learning_rate)
     print("Done")
 
     #                               Summary Part                               #
@@ -240,8 +260,22 @@ def VAE():
 
     #                               Session Part                               #
     print("Setting up Data Reader...")
-    validation_dataset_reader = mt.Dataset(test_dir, FLAGS.val_batch_size, FLAGS.hidden_state_size, FLAGS.predict_size, FLAGS.num_keys, tick_interval, step=FLAGS.slice_step)
-    test_dataset_reader = mt.Dataset(test_dir, FLAGS.test_batch_size, FLAGS.hidden_state_size, FLAGS.predict_size, FLAGS.num_keys, tick_interval, step=FLAGS.slice_step)
+    validation_dataset_reader = mt.Dataset(directory=test_dir,
+                                           batch_size=FLAGS.val_batch_size,
+                                           is_batch_zero_pad=FLAGS.is_batch_zero_pad,
+                                           hidden_state_size=FLAGS.hidden_state_size,
+                                           predict_size=FLAGS.predict_size,
+                                           num_keys=FLAGS.num_keys,
+                                           tick_interval=tick_interval,
+                                           step=FLAGS.slice_step)
+    test_dataset_reader = mt.Dataset(directory=test_dir,
+                                     batch_size=FLAGS.test_batch_size,
+                                     is_batch_zero_pad=FLAGS.is_batch_zero_pad,
+                                     hidden_state_size=FLAGS.hidden_state_size,
+                                     predict_size=FLAGS.predict_size,
+                                     num_keys=FLAGS.num_keys,
+                                     tick_interval=tick_interval,
+                                     step=FLAGS.slice_step)
     print("done")
 
     sess_config = tf.ConfigProto(allow_soft_placement=True, log_device_placement=False)
@@ -255,44 +289,49 @@ def VAE():
         sess.run(tf.global_variables_initializer())  # if the checkpoint doesn't exist, do initialization
 
     if FLAGS.mode == "train":
-        train_dataset_reader = mt.Dataset(train_dir, FLAGS.tr_batch_size, FLAGS.hidden_state_size, FLAGS.predict_size, FLAGS.num_keys, tick_interval, step=FLAGS.slice_step)
+        train_dataset_reader = mt.Dataset(directory=train_dir,
+                                          batch_size=FLAGS.tr_batch_size,
+                                          is_batch_zero_pad=FLAGS.is_batch_zero_pad,
+                                          hidden_state_size=FLAGS.hidden_state_size,
+                                          predict_size=FLAGS.predict_size,
+                                          num_keys=FLAGS.num_keys,
+                                          tick_interval=tick_interval,
+                                          step=FLAGS.slice_step)
         for itr in range(MAX_MAX_EPOCH):
             feed_dict = utils.vae_run_epoch(train_dataset_reader, FLAGS.tr_batch_size, m_train, sess, dropout_rate)
 
             if itr % 10 == 0:
                 train_loss, train_pred = sess.run([m_train.loss, m_train.predict], feed_dict=feed_dict)
-                train_summary_str = sess.run(loss_summary_op, feed_dict={loss_ph:train_loss})
+                train_summary_str = sess.run(loss_summary_op, feed_dict={loss_ph: train_loss})
                 train_summary_writer.add_summary(train_summary_str, itr)
-                print("Step : %d  TRAINING LOSS %g" %(itr, train_loss))
-                    
+                print("Step : %d  TRAINING LOSS %g" % (itr, train_loss))
+
             if itr % 100 == 0:
                 valid_loss, valid_pred = utils.vae_validation(validation_dataset_reader,
-                                                          FLAGS.val_batch_size, m_valid,
-                                                          FLAGS.hidden_state_size,
-                                                          FLAGS.predict_size, sess,
-                                                          logs_dir, itr, tick_interval)
-                valid_summary_str = sess.run(loss_summary_op, feed_dict={loss_ph:train_loss})
+                                                              FLAGS.val_batch_size, m_valid,
+                                                              FLAGS.hidden_state_size,
+                                                              FLAGS.predict_size, sess,
+                                                              logs_dir, itr, tick_interval)
+                valid_summary_str = sess.run(loss_summary_op, feed_dict={loss_ph: train_loss})
                 valid_summary_writer.add_summary(valid_summary_str, itr)
-                print("Step : %d  VALIDATION LOSS %g" %(itr, valid_loss))
+                print("Step : %d  VALIDATION LOSS %g" % (itr, valid_loss))
 
             if itr % 500 == 0:
-                utils.test_model(test_dataset_reader, 
-                                     FLAGS.test_batch_size, m_test, 
-                                     FLAGS.predict_size,
-                                     sess, 
-                                     logs_dir, 
-                                     itr,
-                                     tick_interval, 10)
+                utils.test_model(test_dataset_reader,
+                                 FLAGS.test_batch_size, m_test,
+                                 FLAGS.predict_size,
+                                 sess,
+                                 logs_dir,
+                                 itr,
+                                 tick_interval, 10)
             if itr % 1000 == 0:
                 saver.save(sess, logs_dir + "/model.ckpt", itr)
-                
+
     if FLAGS.mode == "test":
-        utils.test_model(test_dataset_reader, 
-                         FLAGS.test_batch_size, m_test, 
+        utils.test_model(test_dataset_reader,
+                         FLAGS.test_batch_size, m_test,
                          FLAGS.predict_size,
-                         sess, 
-                         logs_dir, 
+                         sess,
+                         logs_dir,
                          9999,
                          tick_interval, 10)
-    
-
